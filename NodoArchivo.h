4b2a85eb12d6268c5ccd219433a7a44c153e2305
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-12-12 14:32:51 -0300

    Add constructor and destructor to NodoArchivo class

--- NodoArchivo.h
+++ NodoArchivo.h
@@ -1,10 +1,11 @@
 #include "NodoGrafo.h"
 
-
 class NodoArchivo: public NodoGrafo { 
 private: 
     int tamaño; 
     int tipo; // 0: Imagen; 1: Documento; 2: ejecutable; 3: Video; 4: Comprimido 
-public: 
-    bool es_directorio();
+public:
+  NodoArchivo(int id, int tamaño, int tipo);
+  ~NodoArchivo();    
+   bool es_directorio();
 }; 
