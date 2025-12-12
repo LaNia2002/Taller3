ca71fbbd204d64245c474c685a3eca2aaa68a3da
LaNia2002 <estefania.ulloa@alumnos.ucn.cl>
2025-12-12 02:25:50 -0300

    Add NodoArchivo class inheriting from NodoGrafo

---
+++ NodoArchivo.h
@@ -0,0 +1,10 @@
+#include "NodoGrafo.h"
+
+
+class NodoArchivo: public NodoGrafo { 
+private: 
+    int tamaño; 
+    int tipo; // 0: Imagen; 1: Documento; 2: ejecutable; 3: Video; 4: Comprimido 
+public: 
+    bool es_directorio();
+}; 
