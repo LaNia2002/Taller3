26d870a521aab8f712a195fdbe120b67948b06af
Jossette12 <jossette.doll@alumnos.ucn.cl>
2025-12-12 14:31:11 -0300

    Refactor NodoDirectorio class structure and methods
    Refactor NodoDirectorio class to manage children nodes with dynamic array and added methods for child management.

--- NodoDirectorio.h
+++ NodoDirectorio.h
@@ -1,8 +1,20 @@
 #include "NodoGrafo.h"
 
+class NodoDirectorio : public NodoGrafo {
+private:
+    int* hijos;
+    int nHijos;
+    int capHijos;
 
-class NodoDirectorio: public NodoGrafo { 
-public: 
-    bool es_directorio();
-    vector<int> lista_hijos; 
-}; 
+    void crecerNumeroDeHijos();
+
+public:
+    NodoDirectorio(int id);
+    ~NodoDirectorio();
+
+    bool es_directorio() override;
+    int* lista_hijos();
+
+    void agregarHijo(int idHijo);
+    int cantidadHijos();
+};
